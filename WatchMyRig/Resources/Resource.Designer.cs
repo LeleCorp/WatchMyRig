1630c37577dbbbfc72440218952c470e4ec3a9cd
Unknown <averlant.antoine@gmail.com>
2017-09-17 11:26:28 +0200

    Reparation .csproj

--- WatchMyRig/Resources/Resource.Designer.cs
+++ WatchMyRig/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace watchmyrig
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::watchmyrig.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
